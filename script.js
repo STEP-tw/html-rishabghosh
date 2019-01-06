52e0677c14739809309031a26b7f1008d8f2cf5c
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-01-06 21:28:53 +0530

    extracted all scripts to scripts.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const removeHeading = function () {
+	let element = document.getElementById("cool_crossbows");
+	let list = document.getElementById("jump_to");
+	element.innerText = "\n";
+	list.style.color = "#303030";
+}
+
+const addHeading = function () {
+	let element = document.getElementById("cool_crossbows");
+	let list = document.getElementById("jump_to");
+	element.innerText = "COOL CROSSBOWS";
+	list.style.color = "#838383";
+}
+
+const makeListVisible = function () {
+	let element = document.getElementById("drop_down");
+	element.style.display = "block";
+}
+
+const makeListVanish = function () {
+	let element = document.getElementById("drop_down");
+	element.style.display = "none";
+}
