18504cb91aeefe09af88373141f4192c780e14c6
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-01-06 22:29:11 +0530

    Added navigate bar

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-const removeHeading = function () {
-	let element = document.getElementById("cool_crossbows");
-	let list = document.getElementById("jump_to");
-	element.innerText = "\n";
-	list.style.color = "#303030";
-}
-
-const addHeading = function () {
-	let element = document.getElementById("cool_crossbows");
-	let list = document.getElementById("jump_to");
-	element.innerText = "COOL CROSSBOWS";
-	list.style.color = "#838383";
-}
-
 const makeListVisible = function () {
 	let element = document.getElementById("drop_down");
 	element.style.display = "block";
