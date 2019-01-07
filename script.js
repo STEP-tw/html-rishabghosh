3ecefa1df69703d55ae35f8045ebfd1e62187739
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-07 06:30:03 +0000

    Deploy STEP-tw/html-rishabghosh to github.com/STEP-tw/html-rishabghosh.git:gh-pages

---
+++ script.js
@@ -0,0 +1,9 @@
+const makeListVisible = function () {
+	let element = document.getElementById("drop_down");
+	element.style.display = "block";
+}
+
+const hideList = function () {
+	let element = document.getElementById("drop_down");
+	element.style.display = "none";
+}
